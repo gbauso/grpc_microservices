653d1605db4d6c59ea523e1361ad4ad4a77fd88b
Gbauso <guilherme.bauso@gmail.com>
2021-01-13 10:28:12 +0100

    add support for cloudampq

--- discovery-service/src/DiscoveryService/ServiceBus/ServiceBusConfiguration.cs
+++ discovery-service/src/DiscoveryService/ServiceBus/ServiceBusConfiguration.cs
@@ -9,5 +9,6 @@ namespace DiscoveryService
         public string Host { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool IsCloudAmpq { get; set; } = false;
     }
 }
