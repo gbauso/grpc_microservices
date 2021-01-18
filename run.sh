ec3fb0bb990385d92dfc40072cdaf2f7bca47eca
gbauso <guilherme.bauso@gmail.com>
2021-01-18 20:11:20 +0100

    adjust shell

--- run.sh
+++ run.sh
@@ -8,12 +8,15 @@ then
     echo "Error: You need to provide an open weather api key"
 fi
 
-istioctl install --set profile=demo -y
+istioctl install -y
 
+if [[ -n $INSTALL_ISTIO_ADDONS ]] && $INSTALL_ISTIO_ADDONS; 
+then
 kubectl apply -f https://github.com/istio/istio/raw/master/samples/addons/jaeger.yaml
 kubectl apply -f https://github.com/istio/istio/raw/master/samples/addons/prometheus.yaml
 kubectl apply -f https://github.com/istio/istio/raw/master/samples/addons/grafana.yaml
 kubectl apply -f https://github.com/istio/istio/raw/master/samples/addons/kiali.yaml
+fi
 
 helm upgrade --install -f ./charts/values.istio.yaml grpc charts --set weatherService.openWeatherId=$OPEN_WEATHER_ID
 
