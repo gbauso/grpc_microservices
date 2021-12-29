392d7ea9a9db0502e5c95ab0f793553d8fc3dc70
gbauso <guilherme.bauso@gmail.com>
2021-12-29 20:42:56 +0100

    Update WeatherServer.kt

--- weather-service/src/main/kotlin/weather/WeatherServer.kt
+++ weather-service/src/main/kotlin/weather/WeatherServer.kt
@@ -2,6 +2,8 @@ package weather
 
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import io.grpc.health.v1.HealthCheckResponse
+import io.grpc.protobuf.services.HealthStatusManager
 import io.grpc.protobuf.services.ProtoReflectionService
 import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
@@ -9,7 +11,6 @@ import org.koin.core.inject
 import weather.di.openWeatherModule
 import weather.interceptors.LoggingInterceptor
 import weather.interceptors.MetricsInterceptor
-import weather.service.HealthCheckService
 import weather.service.WeatherService
 import weather.util.logging.ILogger
 import weather.util.metrics.IMetricsProvider
@@ -23,25 +24,29 @@ class WeatherServer constructor(
     val metricsProvider: IMetricsProvider by inject()
     val secrets: ISecretProvider by inject()
     val port = secrets.getValue("PORT").toInt()
-    var healhCheckService = HealthCheckService()
+    var healthStatusManager = HealthStatusManager()
 
     val server: Server = ServerBuilder
             .forPort(port)
             .addService(WeatherService())
-            .addService(healhCheckService)
+            .addService(healthStatusManager.healthService)
             .addService(ProtoReflectionService.newInstance())
             .intercept(LoggingInterceptor(logger))
             .intercept(MetricsInterceptor(metricsProvider))
             .build()
 
     fun start() {
+        healthStatusManager.setStatus("cityinformation.CityService", HealthCheckResponse.ServingStatus.SERVING)
+        healthStatusManager.setStatus("grpc.health.v1.Health", HealthCheckResponse.ServingStatus.SERVING)
+        healthStatusManager.setStatus("grpc.reflection.v1alpha.ServerReflection", HealthCheckResponse.ServingStatus.SERVING)
+
         server.start()
         logger.info("Server started, listening on $port")
         Runtime.getRuntime().addShutdownHook(
                 Thread {
                     logger.info("*** shutting down gRPC server since JVM is shutting down")
-                    healhCheckService.onExit()
                     this@WeatherServer.stop()
+                    healthStatusManager.enterTerminalState()
                     logger.info("*** server shut down")
                 }
         )
