eec4b75271a005fd53fb8b33fdbd24ef84e24b3b
Guilherme Bauso <guilherme.bauso@gmail.com>
2022-06-08 16:02:16 +0200

    chore: add more arguments to scripts

--- run.ps1
+++ run.ps1
@@ -1,8 +1,20 @@
 $open_weather_id=$args[0]
-$install_istio_addons=[bool]$args[1]
+$new_relic_url=$args[1]
+$new_relic_key=$args[2]
+$install_istio_addons=[bool]$args[3]
 
 if ( -not $open_weather_id ) {
     Write-Output "Error: You need to provide an open weather api key"
+    exit 
+}
+
+if ( -not $new_relic_url ) {
+    Write-Output "Error: You need to provide the new relic logging url"
+    exit 
+}
+
+if ( -not $new_relic_key ) {
+    Write-Output "Error: You need to provide a new relic api key"
     exit 
 }
 
@@ -21,7 +33,10 @@ catch {
 }
 
 try {
-    helm upgrade --install -f ./charts/values.istio.yaml grpc charts --set weatherService.openWeatherId=$open_weather_id
+    helm upgrade --install -f ./charts/values.istio.yaml grpc charts \
+    --set weatherService.openWeatherId=$open_weather_id \
+    --set newRelic.apiKey=$new_relic_key \
+    --set newRelic.baseUrl=$new_relic_url
 }
 catch {
     Write-Output "Error: You don't have helm charts installed"
--- run.sh
+++ run.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 OPEN_WEATHER_ID=$1
-INSTALL_ISTIO_ADDONS=$2
+NEW_RELIC_KEY=$2
+NEW_RELIC_URL=$3
+INSTALL_ISTIO_ADDONS=$4
 
 if [ -z "$OPEN_WEATHER_ID" ]; 
 then
     echo "Error: You need to provide an open weather api key"
 fi
+
+if [ -z "$NEW_RELIC_KEY" ]; 
+then
+    echo "Error: You need to provide a new relic api key"
+fi
+
+if [ -z "$NEW_RELIC_URL" ]; 
+then
+    echo "Error: You need to provide the new relic logging url"
+fi
+
 
 istioctl install -y
 
@@ -18,5 +31,8 @@ then
     kubectl apply -f https://github.com/istio/istio/raw/master/samples/addons/kiali.yaml
 fi
 
-helm upgrade --install -f ./charts/values.istio.yaml grpc charts --set weatherService.openWeatherId=$OPEN_WEATHER_ID
+helm upgrade --install -f ./charts/values.istio.yaml grpc charts \
+--set weatherService.openWeatherId=$OPEN_WEATHER_ID \
+--set newRelic.apiKey=$NEW_RELIC_KEY \
+--set newRelic.baseUrl=$NEW_RELIC_URL
 
